fb8304efeda5489513cbff0161ac55ee53898c7c
Mark Khrizman <mark.kh@cookies.games>
2024-06-27 04:54:23 +0300

    Constants
    Added audio and save constants

---
+++ Assets/Scripts/Constants/AudioConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Constants
+{
+    public class AudioConstants
+    {
+        public const string MUSIC_VOLUME = "MusicVolume";
+        public const string SOUND_VOLUME = "SoundVolume";
+        public const string MASTER_PITCH = "MasterPitch";
+    }
+}
---
+++ Assets/Scripts/Constants/KeyConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Constants
+{
+    public class KeyConstants
+    {
+        public const string PLAYER_DATA = "player_data";
+        public const string AUDIO_DATA = "audio_data";
+    }
+}
