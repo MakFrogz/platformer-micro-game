c21a9ef506163db8a1680deeb7b796684dd37dfb
Mark Khrizman <mark.kh@cookies.games>
2024-06-27 04:55:39 +0300

    Audio
    Added audio service

---
+++ Assets/Scripts/Audio/AudioService.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Audio;
+
+namespace Audio
+{
+    public class AudioService : IAudioService
+    {
+        private AudioMixer _audioMixer;
+
+        public AudioService(AudioMixer audioMixer)
+        {
+            _audioMixer = audioMixer;
+        }
+        
+        public void Mute(string exposedParameter, bool mute)
+        {
+            float volume = mute ? -80 : 0;
+            _audioMixer.SetFloat(exposedParameter, volume);
+        }
+
+        public void SetPitch(string exposedParameter, float pitch)
+        {
+            _audioMixer.SetFloat(exposedParameter, pitch);
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/IAudioService.cs
@@ -0,0 +1,8 @@
+﻿namespace Audio
+{
+    public interface IAudioService
+    {
+        void Mute(string exposedParameter, bool mute);
+        void SetPitch(string exposedParameter, float pitch);
+    }
+}
