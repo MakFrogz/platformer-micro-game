f6fb8f9393b4fa89f08fe1170845dc76ef4bdd06
Mark Khrizman <mark.kh@cookies.games>
2024-06-27 04:56:13 +0300

    Dostance
    Added distance handler

---
+++ Assets/Scripts/Mechanics/Distance/DistanceHandler.cs
@@ -0,0 +1,26 @@
+﻿using Model;
+using UnityEngine;
+using VContainer;
+
+namespace Mechanics.Distance
+{
+    public class DistanceHandler : MonoBehaviour
+    {
+        [SerializeField] private Transform _spawnPoint;
+        
+        private IDistanceModel _distanceModel;
+        private float _distance;
+
+        [Inject]
+        private void Construct(IDistanceModel distanceModel)
+        {
+            _distanceModel = distanceModel;
+        }
+
+        private void Update()
+        {
+            _distance = Vector2.Distance(transform.position, _spawnPoint.position);
+            _distanceModel.SetDistance(Mathf.FloorToInt(_distance));
+        }
+    }
+}
